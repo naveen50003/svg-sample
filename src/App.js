e0eb94e7d8a90a5863fa7361839949b4be781d88
Karumuru Navaneeth <naveen50003@gmail.com>
2022-10-26 23:30:21 +0530

    implemented basic color selection logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import logo, { ReactComponent as Image } from './logo.svg';
 import './App.css';
 
 function App() {
+  const activateLasers = (event) => {
+    console.log(event)
+    event.target.style="fill:#66ff66"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Image onClick={(event) => activateLasers(event)}/> 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
